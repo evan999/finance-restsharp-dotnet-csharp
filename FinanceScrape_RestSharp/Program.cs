44cb9db9836b1c2d6437672a43edcc8ced172800
evan999 <emeshberge@gmail.com>
2019-08-12 20:12:19 -0400

    Prepare Rest Request and Response. Fetches finance data from RapidAPI MorningStar Finance API.

--- FinanceScrape_RestSharp/Program.cs
+++ FinanceScrape_RestSharp/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http;
+using RestSharp;
 
 namespace FinanceScrape_RestSharp
 {
@@ -10,6 +12,18 @@ namespace FinanceScrape_RestSharp
     {
         static void Main(string[] args)
         {
+            RestClient client = new RestClient("https://morning-star.p.rapidapi.com");
+
+            RestRequest request = new RestRequest("/market/get-summary", Method.GET);
+            request.AddHeader("x-rapidapi-host", "morning-star.p.rapidapi.com");
+            request.AddHeader("x-rapidapi-key", "fb96429169msh172f9e8369dbb6dp11b294jsn617d9bce3a4d");
+
+            IRestResponse response = client.Execute(request);
+            var content = response.Content;
+
+            Console.WriteLine(content);
+            Console.ReadLine();
+
         }
     }
 }
